7dce0623cbdd0f582977507f77dc7bc83e648497
Matthew Daly <matthewbdaly@gmail.com>
2017-11-17 16:30:30 +0000

    Added failing integration test for submitting a snapshot

--- tests/Feature/SnapshotTest.php
+++ tests/Feature/SnapshotTest.php
@@ -7,7 +7,33 @@ use Matthewbdaly\LaravelErrorSnapshot\Eloquent\Models\Snapshot;
 
 class SnapshotTest extends BrowserKitTestCase
 {
-    public function testCreateSnapshot()
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testCreateSnapshot($data)
     {
+        $user = factory(\Tests\Fixtures\User::class)->create();
+        $this->be($user);
+        $response = $this->call(
+            'POST',
+            '/api/snapshot',
+            $data
+        );
+        $this->assertResponseStatus(201);
+        $snapshots = Snapshot::all();
+        $this->assertCount(1, $snapshots);
+        $saved = $snapshots[0];
+        $this->assertEquals(json_encode(['valid' => true]), $saved->state);
+        $this->assertEquals(json_encode(['valid' => true]), $saved->trace);
+        $this->assertEquals(json_encode(['valid' => true]), $saved->meta);
+    }
+
+    public function dataProvider()
+    {
+        return [[[
+            'state' => json_encode(['valid' => true]),
+            'trace' => json_encode(['valid' => true]),
+            'meta' => json_encode(['valid' => true]),
+        ]]];
     }
 }
