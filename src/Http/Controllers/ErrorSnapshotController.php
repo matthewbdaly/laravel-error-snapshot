80cb69e92e3979df3b0d75013c83cdf17f4a4895
Matthew Daly <matthewbdaly@gmail.com>
2017-11-17 15:14:38 +0000

    Removed more unwanted controller methods

--- src/Http/Controllers/ErrorSnapshotController.php
+++ src/Http/Controllers/ErrorSnapshotController.php
@@ -45,27 +45,4 @@ class ErrorSnapshotController extends BaseController
     {
         //
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
