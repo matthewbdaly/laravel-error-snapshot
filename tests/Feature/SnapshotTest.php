50d2a4f0d730df83f61dccd69419a846ce8e63e0
Matthew Daly <matthewbdaly@gmail.com>
2017-11-17 16:22:49 +0000

    Added outline of feature test

---
+++ tests/Feature/SnapshotTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\BrowserKitTestCase;
+use Matthewbdaly\LaravelErrorSnapshot\Eloquent\Models\Snapshot;
+
+class SnapshotTest extends BrowserKitTestCase
+{
+    public function testCreateSnapshot()
+    {
+    }
+}
