0acebd45f0739db21bfd30154eb2f58aa862a264
Matthew Daly <matthewbdaly@gmail.com>
2017-11-17 15:59:29 +0000

    Fixed the no assertions issue

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,22 +3,19 @@
 namespace Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Mockery as m;
 
 class TestCase extends BaseTestCase
 {
+    use MockeryPHPUnitIntegration;
+
     public function setUp()
     {
         parent::setUp();
         $this->artisan('migrate', ['--database' => 'sqlite']);
         $this->loadLaravelMigrations(['--database' => 'sqlite']);
         $this->withFactories(__DIR__.'/factories');
-    }
-
-    public function tearDown()
-    {
-        m::close();
-        parent::tearDown();
     }
 
 	protected function getPackageProviders($app)
