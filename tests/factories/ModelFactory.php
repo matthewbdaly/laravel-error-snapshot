fd1f8abd2ffcc4668f10b3b415d210f46e4063de
Matthew Daly <matthewbdaly@gmail.com>
2017-11-17 16:24:55 +0000

    Added user model for fixtures

--- tests/factories/ModelFactory.php
+++ tests/factories/ModelFactory.php
@@ -13,6 +13,17 @@ use Faker\Generator as Faker;
 |
 */
 
+$factory->define(Tests\Fixtures\User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
+
 $factory->define(Matthewbdaly\LaravelErrorSnapshot\Eloquent\Models\Snapshot::class, function (Faker $faker) {
     return [
     ];
